d62362f45519ba82ac14ecb0515635a9fa3f50e8
umby213 <anakin@localhost.localdomain>
2014-10-17 21:04:50 +0200

    3.1 finito

--- Lab03/Lab3-1/main.c
+++ Lab03/Lab3-1/main.c
@@ -3,54 +3,70 @@
 #include <math.h>
 // prototipi
 void distanza(float *X1, float *Y1, float *X2, float *Y2, float *dist);
+void SelectionSort(float vett[], int l, int r);
 
 //struttura dati
 struct {
-    float x, y/*, dist*/;
+    float x, y, dist_0;
 } *punto;
 
 void distanza(float *X1, float *Y1, float *X2, float *Y2, float *dist){
     *dist=sqrt(pow(*X1-*X2,2)+pow(*Y1-*Y2,2));
 }// void distanza
 
+void SelectionSort(float vett[], int l, int r){
+    int i, j, min;
+    float temp;
+    for(i=l; i<r; i++) {
+        min = i;
+        for (j=i+1; j<=r;j++)
+            if ((vett[j] < vett[min]))
+                min = j;
+        temp = vett[i];
+        vett[i] = vett[min];
+        vett[min] = temp;
+    }
+}// void SelectionSort
 
 int main(int argc, char *argv[]){
     if (argc != 4) return 1; // controllo correttezza riga di comando
-    float min, max, dist;
-    FILE *f_in, *f_out;
+    float min, max, dist, *vett;
+    FILE *f_in=NULL, *f_out=NULL;
     int num_punti=0, i, j, p1, p2, p3, p4;
 
     f_in=fopen(argv[1], "r");
-    while (fscanf(f_in, "%f %f", &min, &max) != EOF) num_punti++;
+    while (fscanf(f_in, "%f %f", &min, &max) != EOF) num_punti++; //conto il numero dei punti
     rewind(f_in); // rileggo il file dall'inizio
-    punto=malloc(num_punti*sizeof(punto));
+    punto=malloc((num_punti+1)*sizeof(punto));  // malloc della struct
+    vett=malloc(num_punti*sizeof(float));       // malloc di vett
+    punto[num_punti+1].x=punto[num_punti+1].y=0; // origine
     for (i=0; i<num_punti; i++){
-        fscanf(f_in, "%f %f", &punto[i].x, &punto[i].y);
+        fscanf(f_in, "%f %f", &punto[i].x, &punto[i].y); // salvo tutti i punti
     }//for i
     min=max=0;
     printf("Lista dei punti con distanza minore di %d:\n", atoi(argv[2]));
     for (i=0; i<num_punti; i++){
+        distanza(&punto[i].x, &punto[i].y, &punto[num_punti+1].x, &punto[num_punti+1].y, &punto[i].dist_0); // calcolo la distanza da zero
+        vett[i]=punto[i].dist_0; // copio in un vettore per passarlo agevolmente a SelectionSort();
         for (j=i+1; j<num_punti; j++){
-            distanza(&punto[i].x, &punto[i].y, &punto[j].x, &punto[j].y, &dist);
+            distanza(&punto[i].x, &punto[i].y, &punto[j].x, &punto[j].y, &dist); // calcolo la distanza fra tutti i punti
             if ((i==0 && j==1) || dist<min){
-                min=dist; p1=i; p2=j;
+                min=dist; p1=i; p2=j;   // salvo distanza minima e punti
             }
             if (((i==0 && j==1)) || dist>max){
-                max=dist; p3=i; p4=j;
+                max=dist; p3=i; p4=j;  // salvo distanza massima e punti
             }
-            if (dist <= atof(argv[2])){
+            if (dist<=atof(argv[2])){  // stampo tutte le coppie che hanno una distanza inferiore ad argv[2]
                 printf("P%d=(%.1f, %.1f) P%d=(%.1f, %.1f) dist=%.2f \n", i, punto[i].x, punto[i].y, j, punto[j].x, punto[j].y, dist);
             }
         }
     }//for i
-    printf("i punti a distanza minore sono (%d) e (%d) che distano %.2f\n", p1, p2, min);
-    printf("i punti a distanza maggiore sono (%d) e (%d) che distano %.2f\n", p3, p4, max);
-
+    printf("\nI punti a distanza minore sono (%d) e (%d) che distano %.2f\n", p1, p2, min);
+    printf("I punti a distanza maggiore sono (%d) e (%d) che distano %.2f\n", p3, p4, max);
+    SelectionSort(vett, 0, num_punti-1); // ordino
     f_out=fopen(argv[3], "w");
-
-
-    printf("Hello world!\n");
-    fclose(f_in);
-    fclose(f_out);
+    for (i=0;i<num_punti;i++){
+        fprintf(f_out, "%.2f\n", vett[i]); // stampa ordinata nel file
+    }//for i
     return 0;
 }//main
