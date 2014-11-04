458829c20c462d17333aebbff79b1044fd586f7e
umby213 <realeumberto@gmail.com>
2014-11-04 16:10:22 +0100

    5.2

---
+++ Lab05/Lab5_2/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void usage(char*);
+void catalan(int, int *);
+
+void usage(char *str){
+  printf("Uso: %s <n>, dove n è l'ennesimo numero di Catalan.\n", str);  
+}// void usage ()
+
+void catalan(int n, int *v){
+	int j;
+	v[0]=1;
+	if (n>0) 
+		catalan(n-1, v); // scomposizione del problema
+	for (j=0; j<n; j++)
+		v[n]=v[n]+(v[j]*v[n-1-j]);
+	return;
+}
+
+
+
+int main(int argc, char **argv) {
+	if (argc != 2){
+	    usage(argv[0]);
+    	return -1;
+	}
+	int N=atoi(argv[1]), *vett;
+	vett=malloc(N*sizeof(int));
+	catalan(N, vett);
+	
+	int i;
+	for (i=0; i<N; i++) printf("%d \n", vett[i] );
+
+	printf ("Il Catalan di %d è: %d.\n", N, vett[N-1]);
+	return 0;
+}//main
