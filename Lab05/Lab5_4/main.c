be8b4051624fa28ba4c935351bcfab15f599ef50
umby213 <realeumberto@gmail.com>
2014-11-09 15:29:09 +0100

    5_4, inserimento in lista e stampa ok

--- Lab05/Lab5_4/main.c
+++ Lab05/Lab5_4/main.c
@@ -22,38 +22,60 @@
 #include <ctype.h>
 #include <string.h>
 
-void usage ();
+
+/*-----------------------------------------------------------------------------
+ *  Data Struct
+ *-----------------------------------------------------------------------------*/
+typedef struct nodo *link;
+struct nodo{
+	char cod[11];
+	link next;	// link al nodo successivo
+};
+
+/*-----------------------------------------------------------------------------
+ *  Proto
+ *-----------------------------------------------------------------------------*/
+void usage (int);
+link add_node (char *);  
+void print_all (link);
+void print_first ();
+
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  main
  *  Description:  
  * =====================================================================================
  */
+link head=NULL;	// puntatore in testa alla lista.
+
 int main ( int argc, char **argv ){
 	int continua=1;
+	char stringa[11];
 	char *comando=malloc(sizeof(char));
-	
 	system("clear");
-	usage();
+	usage(0);
 	while ( continua ) {
 		printf (">> ");
 		scanf ("%s", comando);
 		if (strlen(comando)!=1) {
-			usage();		// il comando non può essere più lungo di una lettera!
+			usage(1);		// il comando non può essere più lungo di una lettera!
 			continue;	// quindi se lo è salta all'iterazione successiva dopo aver lanciato usage();.
 		}
 		comando[0]=toupper(comando[0]);
 		if ( comando[0] == 'A'  ){
-			printf("Aggiungo in coda\n");
+//			printf("Aggiungo nodo alla lista (in coda):\n");
+			printf(">> <Adding node function> >> Inserire codice di 10 cifre: ");
+			scanf("%s", stringa);
+			head=add_node(stringa);
 		}else	if ( comando[0] == 'S'  ){
 			printf("Stampo il primo della lista\n");
 		}else	if ( comando[0] == 'T'  ){
-			printf("Stampo tutti\n");
+			print_all(head);
 		}else	if ( comando[0] == 'E'  ){
 			printf("Esco\n");
 			return EXIT_SUCCESS;
 		}else{
-			usage();
+			usage(1);
 		}
 	}// while ( continua )
 	return EXIT_SUCCESS;
@@ -62,9 +84,60 @@ int main ( int argc, char **argv ){
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  usage
- *  Description:  
+ *  Description:  Simply printf the program "syntax" 
  * =====================================================================================
  */
-void usage (){
+void usage (int a){
+	if (a) printf("Comando non trovato!\n");
 	printf("<<< [A]ggiungi in coda, [S]tampa il primo, s[T]ampa tutti, [E]sci >>>\n");
 }		/* -----  end of function usage  ----- */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  add_node
+ *  Description:  aggiunge nodo alla lista, controlla che la lista non sia vuota
+ *  					in tal caso la inizializza.
+ * =====================================================================================
+ */
+link add_node (char *str){
+	link k, tmp=malloc(sizeof *tmp);
+	tmp->next=NULL;
+	strcpy(tmp->cod,str);
+	if ( head == NULL )	{
+		head=tmp;
+	}else{
+		k=malloc(sizeof *k);
+		// cerco il nodo finale a al suo ->next pianto tmp
+		for (k=head; k->next!=NULL; k=k->next);
+		k->next=tmp;	
+	}
+	return head;
+}		/* -----  end of function add_node  ----- */
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  print_all
+ *  Description:  printf all list 
+ * =====================================================================================
+ */
+void print_all (link t){
+	int i=1;
+	 
+	for (t=head ; t!=NULL ; t=t->next){
+		printf (">> <printing function> >> %2d) %s\n", i++, t->cod);
+		//t=t->next;
+	}	
+	return;
+}		/* -----  end of function print_all  ----- */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  print_first
+ *  Description:  print & delete first entry in list
+ * =====================================================================================
+ */
+void print_first ( ){
+	return;
+}		/* -----  end of function print_first  ----- */
+
